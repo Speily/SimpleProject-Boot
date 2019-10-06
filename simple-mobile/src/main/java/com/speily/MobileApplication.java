9d0450a41c9f6663d87aef2ea1922deccf43b733
SPL <speily@126.com>
2019-10-06 14:53:19 +0800

    增加移动端模块，next清除耦合的配置，使mobile和manager互相独立，共享service、common、mapper。

---
+++ simple-mobile/src/main/java/com/speily/MobileApplication.java
@@ -0,0 +1,16 @@
+package com.speily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class MobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileApplication.class, args);
+    }
+
+}
